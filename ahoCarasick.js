d20633424ecfe5e6eeca03210994e35b8b9e8b0e
Balint Kardos <kard.balint@gmail.com>
2023-11-14 12:29:16 +0100

    ahoCorasick algoritmus

--- ahoCarasick.js
+++ ahoCarasick.js
@@ -1,1 +1,102 @@
-//TODO
+class TrieNode {
+    constructor() {
+      this.children = {};
+      this.output = [];
+      this.failure = null;
+    }
+  }
+  
+  class AhoCorasick {
+    constructor() {
+      this.root = new TrieNode();
+    }
+  
+    addPattern(pattern) {
+      let node = this.root;
+      for (let char of pattern) {
+        if (!node.children[char]) {
+          node.children[char] = new TrieNode();
+        }
+        node = node.children[char];
+      }
+      node.output.push(pattern);
+    }
+  
+    buildFailureLinks() {
+      const queue = [];
+  
+      // Set failure links for depth 1 (child nodes of the root)
+      for (const key in this.root.children) {
+        const child = this.root.children[key];
+        child.failure = this.root;
+        queue.push(child);
+      }
+  
+      // Set failure links using BFS
+      while (queue.length > 0) {
+        const currentNode = queue.shift();
+  
+        for (const key in currentNode.children) {
+          const child = currentNode.children[key];
+          queue.push(child);
+  
+          let failureNode = currentNode.failure;
+  
+          while (failureNode !== null && !failureNode.children[key]) {
+            failureNode = failureNode.failure;
+          }
+  
+          child.failure = failureNode ? failureNode.children[key] : this.root;
+  
+          child.output = child.output.concat(child.failure.output);
+        }
+      }
+    }
+  
+    search(text) {
+      let node = this.root;
+      const results = [];
+  
+      for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+  
+        while (node !== this.root && !node.children[char]) {
+          node = node.failure;
+        }
+  
+        if (node.children[char]) {
+          node = node.children[char];
+        } else {
+          node = this.root;
+        }
+  
+        for (const pattern of node.output) {
+          results.push({ pattern, index: i - pattern.length + 1 });
+        }
+      }
+  
+      return results;
+    }
+  }
+  
+  // Példa használat:
+  const ac = new AhoCorasick();
+  
+  ac.addPattern("he");
+  ac.addPattern("she");
+  ac.addPattern("his");
+  ac.addPattern("hers");
+  
+  ac.buildFailureLinks();
+  
+  const text = "hershey";
+  const matches = ac.search(text);
+  
+  console.log(`Text: ${text}`);
+  console.log("Matches:");
+  matches.forEach(match => {
+    console.log(`Pattern: ${match.pattern}, Index: ${match.index}`);
+  });
+
+  //https://www.geeksforgeeks.org/aho-corasick-algorithm-pattern-searching/
+  
--- kmpPrefix.js
+++ kmpPrefix.js
@@ -1,11 +1,11 @@
 function prefixFunction(s) {
-    let p = new Array(s.length).fill(0);
+    let p = [0];
     let j = 0;
     for (let i = 1; i < s.length; i++) {
-      while (j > 0 && s.charAt(j) !== s.charAt(i)) {
+      while (j > 0 && s[j] !== s[i]) {
         j = p[j-1];
       }
-      if (s.charAt(j) === s.charAt(i)) {
+      if (s[j] === s[i]) {
         j++;
       }
       p[i] = j;
