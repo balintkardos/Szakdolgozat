42c192b4a20ae1aabd43435c7f1c51f9eba49fd0
Balint Kardos <kard.balint@gmail.com>
2023-11-18 19:37:04 +0100

    mai dolgok szarok mind

--- main.js
+++ main.js
@@ -3,11 +3,37 @@ const naiv = require('./naiv');
 const shiftOr = require('./shiftAnd');
 const notSoNaive = require('./notSoNaive');
 const KR = require('./rubinKarp');
+const SuffixTree = require('./suffixTree');
 
 function measureMemoryUsage() {
     const used = process.memoryUsage();
     console.log(`Memory Usage: ${JSON.stringify(used)}`);
 }
+function isStringInTree(tree, P) {
+    let currentNode = tree.head;
+  
+    for (let i = 0; i < P.length; i++) {
+      let found = false;
+  
+      // Check among child nodes
+      for (let j = 0; j < currentNode.nodes.length; j++) {
+        const node = currentNode.nodes[j];
+        if (node.value === P[i]) {
+          currentNode = node;
+          found = true;
+          break;
+        }
+      }
+  
+      if (!found) {
+        // If no matching child node is found, the string is not in the tree
+        return false;
+      }
+    }
+  
+    // If the loop completes, the entire string is present in the tree
+    return true;
+  }
 
 
 
@@ -26,3 +52,15 @@ console.timeEnd("notSoNaive");
 console.time("RK");
 console.log(KR("abc", "almapapriabckaáabc"));
 console.timeEnd("RK");
+
+const suffixTree = new SuffixTree();
+suffixTree.add("aaaabbb");
+const longestSubstring = suffixTree.getLongestSubstring();
+console.log("Longest Substring:", longestSubstring);
+
+
+
+
+const searchString = "aab";
+const isStringPresent = isStringInTree(suffixTree, searchString);
+console.log(`String "${searchString}" is${isStringPresent ? '' : ' not'} in the tree.`);
